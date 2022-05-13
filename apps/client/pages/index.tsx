cdf6716d34717b13f6d88e15e4c4998c02590486
Jack Allen <jackallensemail+git@gmail.com>
2022-05-14 00:44:51 +0100

    mobile

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classnames from 'classnames';
-import { BookmarkIcon, HeartIcon as HeartOutlineIcon, UserCircleIcon } from "@heroicons/react/outline";
-import { BadgeCheckIcon, HeartIcon, StarIcon, MenuIcon } from "@heroicons/react/solid";
+import { ExternalLinkIcon, HeartIcon as HeartOutlineIcon, UserCircleIcon } from "@heroicons/react/outline";
+import { BadgeCheckIcon, ChevronDoubleUpIcon, HeartIcon, MenuIcon, StarIcon } from "@heroicons/react/solid";
 
 function Container({ children, className }: React.PropsWithChildren<{ className?: string }>) {
   return (
@@ -33,7 +33,7 @@ function HomePage() {
             </nav>
 
             <div className="md:hidden">
-              <MenuIcon className="w-6 h-6 "/>
+              <MenuIcon className="w-6 h-6 " />
             </div>
           </div>
 
@@ -45,18 +45,28 @@ function HomePage() {
         <div className="container items-center max-w-6xl px-8 mx-auto xl:px-5">
           <div className="flex flex-wrap items-center sm:-mx-3">
             <div className="w-full md:w-1/2 md:px-3">
-              <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
-                <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
+              <div
+                className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
+                <h1
+                  className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
                   <span className="block xl:inline">Level Up Your</span>
                   <span className="block text-indigo-600">Job Search</span>
                 </h1>
-                <p className="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">We help awesome contract devs find amazing clients without recruiters. Create your profile with your availability and IR35 preference to start receiving relevant opportunities.</p>
+                <p className="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">We help awesome
+                  contract devs find amazing clients without recruiters. Create your profile with your availability and
+                  IR35 preference to start receiving relevant opportunities.</p>
                 <div className="relative flex flex-col sm:flex-row sm:space-x-4">
-                  <a href="#_" className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
+                  <a href="#_"
+                     className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
                     Try It Free
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                      <line x1="5" y1="12" x2="19" y2="12"></line>
+                      <polyline points="12 5 19 12 12 19"></polyline>
+                    </svg>
                   </a>
-                  <a href="#_" className="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600">
+                  <a href="#_"
+                     className="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600">
                     Learn More
                   </a>
                 </div>
@@ -64,7 +74,7 @@ function HomePage() {
             </div>
             <div className="w-full md:w-1/2">
               <div className="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
-                <img src="https://cdn.devdojo.com/images/november2020/hero-image.jpeg"/>
+                <img src="https://cdn.devdojo.com/images/november2020/hero-image.jpeg" />
               </div>
             </div>
           </div>
@@ -126,15 +136,23 @@ function HomePage() {
                     src="https://wellpaidio.imgix.net/logos/327c5998-1553-4b9a-bb31-73e936fd5411.png?w=236&h=236&fit=crop" />
                 </div>
                 <div className="flex-1 overflow-hidden">
-                  <div className="text-lg font-semibold overflow-hidden text-ellipsis">Senior React
-                    Developer
+                  <div className="text-lg flex items-center font-semibold overflow-hidden text-ellipsis">
+                    <span>Senior React Developer</span>
+                    <BadgeCheckIcon className="w-5 h-5 text-blue-400 ml-1" />
                   </div>
-                  <div className="text-xs text-gray-400 overflow-hidden text-ellipsis">We are looking
-                    for a Senior React Developer to join our team of highly
+                  <div className="text-xs text-gray-400 overflow-hidden text-ellipsis">
+                    London, UK
+                  </div>
+                  <div className="flex hidden">
+                    <div
+                      className="flex items-center text-xs px-1 py-0.5 text-yellow-800 mt-1 rounded-md bg-yellow-400">
+                      <ChevronDoubleUpIcon className="w-3 h-3 mr-0.5" /> Promoted
+                    </div>
                   </div>
                 </div>
-                <div className="flex-0 flex justify-center items-center border border-gray-100 w-10 h-10 rounded-md">
-                  <BookmarkIcon className="w-4 h-4 text-gray-500" />
+                <div className="flex-0 flex justify-center items-start">
+                  {index % 2 ? <HeartOutlineIcon className="w-7 w-7 text-gray-300" /> :
+                    <HeartIcon className="w-7 w-7 text-red-400" />}
                 </div>
               </div>
 
@@ -155,10 +173,7 @@ function HomePage() {
               </div>
 
               <div className="flex justify-between pt-5">
-                <div>
-                  {index % 2 ? <HeartOutlineIcon className="w-7 w-7 text-gray-300" /> :
-                    <HeartIcon className="w-7 w-7 text-red-400" />}
-                </div>
+
                 <div className="grid gap-1 grid-flow-col hidden">
                   <StarIcon className="w-5 w-5 text-yellow-400" />
                   <StarIcon className="w-5 w-5 text-yellow-400" />
@@ -167,15 +182,20 @@ function HomePage() {
                   <StarIcon className="w-5 w-5 text-gray-200" />
                 </div>
 
-                <div className="flex justify-center items-center text-sm">
-                  <div className="flex">
-                    <BadgeCheckIcon className="w-5 h-5 text-blue-400 mr-1" />
-                    <div className="text-sm text-gray-500">Project Verified</div>
+
+                <div className="" />
+
+                <div className="flex items-center justify-center">
+                  <div className="mr-5 text-sm whitespace-nowrap">
+                    <span className="font-semibold">46</span> Applicants
                   </div>
 
-                  <div className="ml-3">
-                    <div><span className="font-semibold">46</span> Applicants</div>
-                  </div>
+                  <button
+                    className="flex items-center justify-center w-full px-6 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 sm:w-auto">
+                    <span>Apply</span>
+                    <ExternalLinkIcon className="w-4 h-4 ml-2" />
+                  </button>
+
                 </div>
               </div>
             </div>
